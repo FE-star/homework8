7d1361edbf377d05e91d1e905a514d8dbd74604a
blankzz <454257312@qq.com>
2022-08-21 00:18:33 +0800

    feat(exercise3): 完成 Exercise3 - 手写简易 Promise

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const PROMISE_STATUS_ENUM = {
+  PENDING: "pending",
+  FULFILLED: "fulfilled",
+  REJECTED: "rejected"
+}
+
 function myPromise(constructor) {
   let self = this;
 
@@ -8,15 +14,20 @@ function myPromise(constructor) {
   self.reason = undefined;//定义状态为rejected的时候的状态
 
   function resolve(value) {
-
     // TODO resolve如何改变状态及返回结果
-
+    if(self.status === PROMISE_STATUS_ENUM.PENDING) {
+      self.status = PROMISE_STATUS_ENUM.FULFILLED
+      self.value = value
+    }
   }
 
   function reject(reason) {
 
     // TODO reject如何改变状态及返回结果
-
+    if(self.status === PROMISE_STATUS_ENUM.PENDING) {
+      self.status = PROMISE_STATUS_ENUM.REJECTED
+      self.reason = reason
+    }
   }
 
   //捕获构造异常
@@ -36,6 +47,11 @@ function myPromise(constructor) {
 myPromise.prototype.then = function (onFullfilled, onRejected) {
 
   //TODO then如何实现
+  if(this.status === PROMISE_STATUS_ENUM.FULFILLED) {
+    onFullfilled(this.value)
+  } else if (this.status === PROMISE_STATUS_ENUM.REJECTED) {
+    onRejected(this.reason)
+  }
 
 }
 module.exports = myPromise
