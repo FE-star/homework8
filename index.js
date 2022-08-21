15f7168372f1ecb6844437bd914d3af77ca6d9c4
zhen <770226915@qq.com>
2022-08-21 15:10:57 +0800

    promise完成

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
 function myPromise(constructor) {
   let self = this;
 
-  self.status = "pending" //定义状态改变前的初始状态
+  self.status = "pending"; //定义状态改变前的初始状态
 
-  self.value = undefined;//定义状态为resolved的时候的状态
+  self.value = undefined; //定义状态为resolved的时候的状态
 
-  self.reason = undefined;//定义状态为rejected的时候的状态
+  self.reason = undefined; //定义状态为rejected的时候的状态
 
   function resolve(value) {
-
     // TODO resolve如何改变状态及返回结果
-
+    if (self.status === "pending") {
+      self.status = "resolved";
+      self.value = value;
+    }
   }
 
   function reject(reason) {
-
     // TODO reject如何改变状态及返回结果
-
+    if (self.status === "pending") {
+      self.status = "rejected";
+      self.reason = reason;
+    }
   }
 
   //捕获构造异常
 
   try {
-
     constructor(resolve, reject);
-
   } catch (e) {
-
     reject(e);
-
   }
-
 }
 
 myPromise.prototype.then = function (onFullfilled, onRejected) {
-
   //TODO then如何实现
+  let self = this;
+  if (self.status === "resolved") {
+    onFullfilled(self.value);
+  }
+  if (self.status === "rejected") {
+    onRejected(self.reason);
+  }
+};
+module.exports = myPromise;
 
-}
-module.exports = myPromise
+new Promise((resolve, reject) => {
+  resolve("isOk");
+}).then();
