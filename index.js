e822ef491093a7425bb27d048084f06e37590df9
blake.li <blake.li@qunar.com>
2022-08-22 23:54:22 +0800

    增加异步机制&增加fulfilled、rejected调用队列机制

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 const PENDING = "pending";
-const RESOLVED = "resolved";
+const FULFILLED = "fulfilled";
 const REJECTED = "rejected";
 
-function myPromise(constructor) {
+function MyPromise(constructor) {
     let self = this;
 
-
     self.status = PENDING //定义状态改变前的初始状态
-
     self.value = undefined;//定义状态为resolved的时候的状态
-
     self.reason = undefined;//定义状态为rejected的时候的状态
+    self.fulfilledQueue = [];//fulfilled 回调执行函数队列
+    self.rejectedQueue = [];//rejected 回调执行函数队列
 
     function resolve(value) {
-
-        // TODO resolve如何改变状态及返回结果
+        // 状态改变只触发一次（pending-》fulfilled），异步暂时用setTimeout模拟
         if (self.status === PENDING) {
-            self.status = RESOLVED;
-            self.value = value;
+            setTimeout(() => {
+                self.status = FULFILLED;
+                self.value = value;
+
+                for (let fn of self.fulfilledQueue) {
+                    fn(value);
+                }
+            }, 0);
         }
     }
 
     function reject(reason) {
-
-        // TODO reject如何改变状态及返回结果
+        // 状态改变只触发一次（pending-》rejected），异步暂时用setTimeout模拟
         if (self.status === PENDING) {
-            self.status = REJECTED;
-            self.reason = reason;
+            setTimeout(() => {
+                self.status = REJECTED;
+                self.reason = reason;
+
+                for (let fn of self.rejectedQueue) {
+                    fn(reason);
+                }
+            }, 0);
         }
     }
 
@@ -44,12 +53,43 @@ function myPromise(constructor) {
 
 }
 
-myPromise.prototype.then = function (onFulfilled, onRejected) {
-    //TODO then如何实现
-    if (this.status === RESOLVED) {
-        onFulfilled(this.value);
-    } else if (this.status === REJECTED) {
-        onRejected(this.reason);
+MyPromise.prototype.then = function (onFulfilled, onRejected) {
+    if (this.status === PENDING) {
+        let fulfilledQueue = this.fulfilledQueue;
+        let rejectedQueue = this.rejectedQueue;
+
+        return new MyPromise(function (resolve, reject) {
+            fulfilledQueue.push(function (data) {
+                try {
+                    let value = onFulfilled(data);
+                    resolve(value);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+
+            rejectedQueue.push(function (data) {
+                try {
+                    let value = onRejected(data);
+                    resolve(value);
+                } catch (e) {
+                    reject(e);
+                }
+            })
+        });
+    } else {
+        let value = this.value;
+        let reason = this.reason;
+        let isFulfilled = this.status === FULFILLED;
+        return new MyPromise(function (resolve, reject) {
+            try {
+                let v = isFulfilled ? onFulfilled(value) : onRejected(reason);
+                resolve(v);
+            } catch (e) {
+                reject(e);
+            }
+        });
     }
 }
-module.exports = myPromise
+
+module.exports = MyPromise
