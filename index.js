f910bcb40b0f9ad3435027023a42f72c91a9d8c7
blake.li <blake.li@qunar.com>
2022-08-22 22:31:29 +0800

    提交作业

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
+const PENDING = "pending";
+const RESOLVED = "resolved";
+const REJECTED = "rejected";
+
 function myPromise(constructor) {
-  let self = this;
+    let self = this;
 
-  self.status = "pending" //定义状态改变前的初始状态
+    self.status = PENDING //定义状态改变前的初始状态
 
-  self.value = undefined;//定义状态为resolved的时候的状态
+    self.value = undefined;//定义状态为resolved的时候的状态
 
-  self.reason = undefined;//定义状态为rejected的时候的状态
+    self.reason = undefined;//定义状态为rejected的时候的状态
 
-  function resolve(value) {
+    function resolve(value) {
 
-    // TODO resolve如何改变状态及返回结果
+        // TODO resolve如何改变状态及返回结果
+        if (self.status === PENDING) {
+            self.status = RESOLVED;
+            self.value = value;
+        }
+    }
 
-  }
+    function reject(reason) {
 
-  function reject(reason) {
+        // TODO reject如何改变状态及返回结果
+        if (self.status === PENDING) {
+            self.status = REJECTED;
+            self.reason = reason;
+        }
+    }
 
-    // TODO reject如何改变状态及返回结果
+    //捕获构造异常
 
-  }
+    try {
 
-  //捕获构造异常
+        constructor(resolve, reject);
 
-  try {
+    } catch (e) {
 
-    constructor(resolve, reject);
+        reject(e);
 
-  } catch (e) {
-
-    reject(e);
-
-  }
+    }
 
 }
 
-myPromise.prototype.then = function (onFullfilled, onRejected) {
-
-  //TODO then如何实现
-
+myPromise.prototype.then = function (onFulfilled, onRejected) {
+    //TODO then如何实现
+    if (this.status === RESOLVED) {
+        onFulfilled(this.value);
+    } else if (this.status === REJECTED) {
+        onRejected(this.reason);
+    }
 }
 module.exports = myPromise
