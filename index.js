75d578965153b430b4d3fd763fe54f7139971e91
caoxiangsong <caoxiangsong@oureman.com>
2022-08-25 09:24:03 +0800

    课程练习 8：简单实现promise

--- index.js
+++ index.js
@@ -8,34 +8,72 @@ function myPromise(constructor) {
   self.reason = undefined;//定义状态为rejected的时候的状态
 
   function resolve(value) {
-
-    // TODO resolve如何改变状态及返回结果
-
+    self.value = value
+    self.status = 'resolve'
+    const { fullfilled } = self
+    if(fullfilled){
+      fullfilled(self.value)
+    }
   }
 
   function reject(reason) {
 
-    // TODO reject如何改变状态及返回结果
+    self.reason = reason
+    self.status = 'reject'
+    // promise的then函数执行时，this值好像是undefined,所以不能直接this.fullfilled
+    const { rejected } = self
+    if(rejected){
+      rejected(reason)
+    }
 
   }
 
   //捕获构造异常
 
   try {
-
     constructor(resolve, reject);
-
   } catch (e) {
-
     reject(e);
-
   }
 
 }
 
 myPromise.prototype.then = function (onFullfilled, onRejected) {
-
-  //TODO then如何实现
-
+  const { status, value, reason} = this
+  if(!onFullfilled ||  typeof onFullfilled !== 'function') {
+    throw new Error('参数异常，最少需要传入完成函数')
+  }
+  // 已完成状态直接执行完成函数，并且返回新的已完成状态的promise,数据更新为完成函数的返回值
+  if(status == 'resolve'){
+    let result = value
+    if(onFullfilled && typeof onFullfilled == 'function') {
+      result = onFullfilled(value)         
+    }
+    return new myPromise(function(resolve){ resolve(result)})
+  } else if(status == 'reject'){
+    // 错误状态直接执行错误函数，并且返回新的错误状态的promise,使用同一个错误对象
+    if(onRejected && typeof onRejected == 'function') {
+      onRejected(reason)      
+    }
+    return new myPromise(function(_resolve,reject){ reject(reason)})
+  } else {
+    const self = this
+    // 感觉两个promise对象耦合了，不过目前没有更好的思路
+    return new myPromise(function(resolve,reject){
+      // 进行中状态，依然需要返回新的promise对象
+      self.fullfilled = function(value){
+        let result = value 
+        result = onFullfilled(value)
+        resolve(result)
+      }
+      self.rejected = function(reason){
+        if(onRejected && typeof onRejected == 'function') {
+          onRejected(reason)     
+        }
+        reject(reason)
+      }
+    })
+  }
 }
+
 module.exports = myPromise
--- index.test.js
+++ index.test.js
@@ -1,9 +1,100 @@
 const myPromise = require('./index');
 
-var p = new myPromise(function (resolve, reject) { resolve('isOk') });
+var p1 = new myPromise(function (resolve, reject) { resolve('isOk') });
 test('测试primose的then是否成功', () => {
   expect.assertions(1);
-  return p.then(data => {
+  return p1.then(data => {
     expect(data).toBe('isOk');
+  });
+});
+
+var p2 = new myPromise(function (resolve, reject) { 
+  setTimeout(()=> 
+    resolve('isOk')
+  , 1000)
+});
+test('测试异步primose的then是否成功', () => {
+  expect.assertions(1);
+  return p2.then(data => {
+    expect(data).toBe('isOk');
+  });
+});
+
+
+test('测试primose的then函数的错误函数入参', () => {
+  expect.assertions(1);
+  var p3 = new myPromise(function (_resolve, reject) { 
+    reject('isno')
+  });
+  return p3.then(data => {
+    expect(data).toBe('isOk');
+  }, err => {
+    expect(err).toBe('isno');
+  });
+});
+
+var p4 = new myPromise(function (_resolve, reject) { 
+  setTimeout(()=> reject('isno'), 1000)
+});
+test('测试异步primose的then函数的错误函数入参', () => {
+  expect.assertions(1);
+  return p4.then(data => {
+    expect(data).toBe('isOk');
+  }, err => {
+    expect(err).toBe('isno');
+  });
+});
+
+var p5 = new myPromise(function (resolve, reject) { resolve('isOk') });
+test('测试primose的连续then是否成功', () => {
+  expect.assertions(2);
+  return p5.then(data => {
+    expect(data).toBe('isOk');
+  }).then(data => {
+    expect(data).toBe('isOk');
+  });
+});
+
+var p6 = new myPromise(function (resolve, reject) { 
+  setTimeout(()=> resolve('isOk'), 1000)
+});
+test('测试异步primose的连续then是否成功', () => {
+  expect.assertions(2);
+  return p6.then(data => {
+    expect(data).toBe('isOk');
+  }).then(data => {
+    expect(data).toBe('isOk');
+  });
+});
+
+var p7 = new myPromise(function (_resolve, reject) { 
+  reject('isno')
+});
+test('测试primose的连续then函数的错误函数入参', () => {
+  expect.assertions(2);
+  return p7.then(data => {
+    expect(data).toBe('isOk');
+  },err => {
+    expect(err).toBe('isno');
+  }).then(data => {
+    expect(data).toBe('isOk');
+  },err => {
+    expect(err).toBe('isno');
+  });
+});
+
+var p8 = new myPromise(function (_resolve, reject) { 
+  setTimeout(()=> reject('isno'), 1000)
+});
+test('测试异步primose的连续then函数的错误函数入参', () => {
+  expect.assertions(2);
+  return p8.then(data => {
+    expect(data).toBe('isOk');
+  },err => {
+    expect(err).toBe('isno');
+  }).then(data => {
+    expect(data).toBe('isOk');
+  },err => {
+    expect(err).toBe('isno');
   });
 });
