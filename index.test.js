bdbaae857540855f55b83e9c62cda95f655095f4
caoxiangsong <caoxiangsong@oureman.com>
2022-08-25 11:33:25 +0800

    课程练习 8：简单实现promise

--- index.test.js
+++ index.test.js
@@ -1,21 +1,22 @@
 const myPromise = require('./index');
 
-var p1 = new myPromise(function (resolve, reject) { resolve('isOk') });
+const p1 = new myPromise(function (resolve, reject) { resolve('isOk') });
 test('测试primose的then是否成功', () => {
   expect.assertions(1);
-  return p1.then(data => {
+  p1.then(data => {
     expect(data).toBe('isOk');
   });
 });
 
-var p2 = new myPromise(function (resolve, reject) { 
-  setTimeout(()=> 
-    resolve('isOk')
-  , 1000)
-});
-test('测试异步primose的then是否成功', () => {
+
+test('测试异步primose的then是否成功', async () => {
   expect.assertions(1);
-  return p2.then(data => {
+  const p2 = new myPromise(function (resolve, reject) { 
+    setTimeout(()=> 
+      resolve('isOk')
+    , 1000)
+  });
+  await p2.then(data => {
     expect(data).toBe('isOk');
   });
 });
@@ -23,56 +24,58 @@ test('测试异步primose的then是否成功', () => {
 
 test('测试primose的then函数的错误函数入参', () => {
   expect.assertions(1);
-  var p3 = new myPromise(function (_resolve, reject) { 
+  const p3 = new myPromise(function (_resolve, reject) { 
     reject('isno')
   });
-  return p3.then(data => {
+  p3.then(data => {
     expect(data).toBe('isOk');
   }, err => {
     expect(err).toBe('isno');
   });
 });
 
-var p4 = new myPromise(function (_resolve, reject) { 
+const p4 = new myPromise(function (_resolve, reject) { 
   setTimeout(()=> reject('isno'), 1000)
 });
 test('测试异步primose的then函数的错误函数入参', () => {
   expect.assertions(1);
-  return p4.then(data => {
+  p4.then(data => {
     expect(data).toBe('isOk');
   }, err => {
     expect(err).toBe('isno');
   });
 });
 
-var p5 = new myPromise(function (resolve, reject) { resolve('isOk') });
+const p5 = new myPromise(function (resolve, reject) { resolve('isOk') });
 test('测试primose的连续then是否成功', () => {
   expect.assertions(2);
-  return p5.then(data => {
+  p5.then(data => {
     expect(data).toBe('isOk');
+    return 'isOk'
   }).then(data => {
     expect(data).toBe('isOk');
   });
 });
 
-var p6 = new myPromise(function (resolve, reject) { 
+const p6 = new myPromise(function (resolve, reject) { 
   setTimeout(()=> resolve('isOk'), 1000)
 });
 test('测试异步primose的连续then是否成功', () => {
   expect.assertions(2);
-  return p6.then(data => {
+  p6.then(data => {
     expect(data).toBe('isOk');
+    return 'isOk'
   }).then(data => {
     expect(data).toBe('isOk');
   });
 });
 
-var p7 = new myPromise(function (_resolve, reject) { 
+const p7 = new myPromise(function (_resolve, reject) { 
   reject('isno')
 });
 test('测试primose的连续then函数的错误函数入参', () => {
   expect.assertions(2);
-  return p7.then(data => {
+  p7.then(data => {
     expect(data).toBe('isOk');
   },err => {
     expect(err).toBe('isno');
@@ -83,12 +86,12 @@ test('测试primose的连续then函数的错误函数入参', () => {
   });
 });
 
-var p8 = new myPromise(function (_resolve, reject) { 
+const p8 = new myPromise(function (_resolve, reject) { 
   setTimeout(()=> reject('isno'), 1000)
 });
 test('测试异步primose的连续then函数的错误函数入参', () => {
   expect.assertions(2);
-  return p8.then(data => {
+  p8.then(data => {
     expect(data).toBe('isOk');
   },err => {
     expect(err).toBe('isno');
