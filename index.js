b2928a687e5f8cfb67abcca56f609c292edf1149
leopen <790480953@qq.com>
2022-08-27 14:29:01 +0800

    feat: 实现简易promise使单元测试通过

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ function myPromise(constructor) {
   self.reason = undefined;//定义状态为rejected的时候的状态
 
   function resolve(value) {
-
     // TODO resolve如何改变状态及返回结果
-
+    if (self.status === "pending") {
+      self.status = "fulfilled";
+      self.value = value;
+    }
   }
 
   function reject(reason) {
-
     // TODO reject如何改变状态及返回结果
-
+    if (self.status === "pending") {
+      self.status = "rejected";
+      self.reason = reason;
+    }
   }
 
   //捕获构造异常
@@ -36,6 +40,11 @@ function myPromise(constructor) {
 myPromise.prototype.then = function (onFullfilled, onRejected) {
 
   //TODO then如何实现
+  if (this.status === "fulfilled" && onFullfilled) {
+    onFullfilled(this.value);
+  } else if (this.status === "rejectd" && onRejected) {
+    onRejected(this.reason);
+  }
 
 }
 module.exports = myPromise
