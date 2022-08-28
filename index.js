dfb82542c32ced0cdb9c66c09fe3bb961d8785ca
xh12 <1960939421@qq.com>
2022-08-28 07:42:47 +0000

    实现大部分promiseA+规范

--- index.js
+++ index.js
@@ -7,14 +7,23 @@ function myPromise(constructor) {
 
   self.reason = undefined;//定义状态为rejected的时候的状态
 
+  self.rejectCallbackFunc = []; // 失败回调
+
+  self.resolveCallbackFunc = []; // 成功回调
+
+
   function resolve(value) {
 
     // TODO resolve如何改变状态及返回结果
     if (self.status === 'pending') {
       self.value = value
       self.status = 'fulfilled'
+      self.resolveCallbackFunc.forEach(callback => {
+        queueMicrotask(() => {
+          callback(value)
+        })
+      })
     }
-
   }
 
   function reject(reason) {
@@ -23,6 +32,11 @@ function myPromise(constructor) {
     if (self.status === 'pending') {
       self.reason = reason
       self.status = 'rejected'
+      self.rejectCallbackFunc.forEach(callback => {
+        queueMicrotask(() => {
+          callback(reason)
+        })
+      })
     }
   }
 
@@ -41,17 +55,116 @@ function myPromise(constructor) {
 }
 
 myPromise.prototype.then = function (onFullfilled, onRejected) {
-
+  onFullfilled = typeof onFullfilled === 'function' ? onFullfilled : v => v;
+  onRejected = typeof onRejected === 'function' ? onRejected : r => { throw r }
   //TODO then如何实现
-  if (this.status === 'fulfilled') {
-    queueMicrotask(() => {
-      onFullfilled(this.value)
-    })
+  const promise1 = new myPromise((resolve, reject) => {
+    if (this.status === 'fulfilled') {
+      queueMicrotask(() => {
+        try {
+          let x = onFullfilled(this.value);
+          resolvePromise(promise1, x, resolve, reject)
+        } catch(e) {
+          reject(e)
+        }
+      })
+    }
+    if (this.status === 'rejected') {
+      queueMicrotask(() => {
+        try {
+          let x = onRejected(this.reason)
+          resolvePromise(promise1, x, resolve, reject);
+        } catch(e) {
+          reject(e)
+        }
+      })
+    }
+    if (this.status === 'pending') {
+      this.resolveCallbackFunc.push(() => {
+        try {
+          let x = onFullfilled(this.value);
+          resolvePromise(promise1, x, resolve, reject);
+        } catch(e) {
+          reject(e)
+        }
+      })
+      this.rejectCallbackFunc.push(() => {
+        try {
+          let x = onRejected(this.reason);
+          resolvePromise(promise1, x, resolve, reject)
+        } catch(e) {
+          reject(e)
+        }
+      })
+    }
+  })
+  return promise1;
+}
+
+// catch 调用 then 方法
+myPromise.prototype.catch = function(onRejected) {
+  return myPromise.prototype.then.call(myPromise, null, onRejected);
+}
+ 
+// 实现链式调用
+function resolvePromise(promise2, x, resolve, reject) {
+  // 首先是 基础类型 不是函数，不是对象
+  if (x === null ) return resolve(x);
+  if (typeof x !== 'object' && typeof x !== 'function') return resolve(x);
+
+  if (x === promise2) {
+    return reject(new TypeError('Chaining cycle detected for promise'));
   }
-  if (this.status === 'rejected') {
-    queueMicrotask(() => {
-      onRejected(this.reason)
-    })
+  // 如果是promise 
+  if (x instanceof myPromise) {
+    if (x.status === 'pending') {
+      x.then(y => {
+        resolvePromise(promise2, y, resolve, reject)
+      }, reject)
+    } else if(x.status === 'fulfilled') {
+      resolve(x.value)
+    } else if(x.status === 'rejected') {
+      reject(x.reason)
+    }
+  }
+
+  // 函数或者对象
+  if(typeof x === 'object' || typeof x === 'function') {
+    try {
+      var then = x.then;
+    } catch (e) {
+        return reject(e);
+    }
+    if (typeof then === 'function') {
+      let called = false;
+      try {
+        then.call(x, y => {
+          if (called) return;
+          called = true;
+          resolvePromise(promise2, y, resolve, reject);
+        }, r => {
+          if (called) return;
+          called = true;
+          reject(r);
+        })
+      } catch (e) {
+        if (called) return;
+        called = true;
+        reject(e);
+      }
+    } else {
+      resolve(x)
+    }
   }
 }
+
+myPromise.deferred = function() {
+  let result = {};
+  result.promise = new myPromise((resolve, reject) => {
+    result.resolve = resolve
+    result.reject = reject
+  })
+  return result
+}
+
 module.exports = myPromise
